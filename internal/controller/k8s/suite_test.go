1084d3c2841211dbc67d2072b1c9c170ef0cafad
David Sabatie <david.sabatie@notrenet.com>
2025-11-23 23:49:53 +0100

    chore: goreleaser
    Signed-off-by: David Sabatie <david.sabatie@notrenet.com>

--- internal/controller/k8s/suite_test.go
+++ internal/controller/k8s/suite_test.go
@@ -23,7 +23,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/kubecloudscaler/kubecloudscaler/internal/utils"
+	"github.com/kubecloudscaler/kubecloudscaler/test/utils"
 	// +kubebuilder:scaffold:imports
 )
 
